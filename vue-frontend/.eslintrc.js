c528882806ce8709004b5d456d892816aa8fc0d5
Florentin Bürvenich <Florentin@Familie-Buervenich.de>
2020-12-09 18:05:35 +0100

    Configure typescript to work with frontend code sitting in sub-folder

--- vue-frontend/.eslintrc.js
+++ vue-frontend/.eslintrc.js
@@ -8,6 +8,9 @@ module.exports = {
 
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 
   extends: [
@@ -19,6 +22,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // disable default "no-unused-vars"-error and
+    // enable the typescript-version (details: https://stackoverflow.com/questions/55280555/typescript-eslint-eslint-plugin-error-route-is-defined-but-never-used-no-un)
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
 
     quotes: ['error', 'single', { avoidEscape: true }],
   },
