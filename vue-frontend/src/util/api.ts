b055dd6512a3c8f415e2d53706317527acfd8132
Florentin Bürvenich <Florentin@Familie-Buervenich.de>
2021-01-12 16:48:57 +0100

    Remove unnecessary log

--- vue-frontend/src/util/api.ts
+++ vue-frontend/src/util/api.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const API_BASE_URL =
   process.env.NODE_ENV === 'production' ? VUE_APP_API_URL : '/api';
 
-console.log('API URL is: ', API_BASE_URL);
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
