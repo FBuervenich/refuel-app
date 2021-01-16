ca1964455dffdea280bb1ca34d4a36a0bee88116
Florentin Bürvenich <Florentin@Familie-Buervenich.de>
2021-01-16 16:19:29 +0100

    Enable history mode for router, configure Netlify

--- vue-frontend/src/router/index.ts
+++ vue-frontend/src/router/index.ts
@@ -1,31 +1,48 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import Dashboard from '@/views/Dashboard.vue';
+import CallbackPage from '@/views/Callback.vue';
+import ErrorPage from '@/views/Error.vue';
+import { routeGuard } from '@/auth';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    beforeEnter: routeGuard,
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard,
+    beforeEnter: routeGuard,
   },
   {
     path: '/about',
     name: 'About',
+    beforeEnter: routeGuard,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+
+  {
+    path: '/callback',
+    name: 'Callback',
+    component: CallbackPage,
+  },
+  {
+    path: '/error',
+    name: 'Error',
+    component: ErrorPage,
+  },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
